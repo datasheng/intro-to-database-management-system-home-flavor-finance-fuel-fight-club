ffb95538ff6e65be5f1d55faa1eab3d615901ea7
SultanaB16 <158234527+SultanaB16@users.noreply.github.com>
2024-05-12 08:15:00 +0500

    FinancefuelIntro

---
+++ FinancialFuelIntro/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        aerotow: ['Aerotow', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
